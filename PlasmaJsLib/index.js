b1d0fc038e5c53e8f724a032ffb67d8effc6276d
Ir1ha <rishatgilmanov1@gmail.com>
2018-12-29 18:08:24 +0300

    Added index.js for npm module

---
+++ PlasmaJsLib/index.js
@@ -0,0 +1,6 @@
+// @create-index
+
+export { default as PlasmaErrors } from './PlasmaService/PlasmaErrors.js';
+export { default as PlasmaService } from './PlasmaService/PlasmaService.js';
+export { default as PlasmaTransaction } from './PlasmaService/PlasmaTransaction.js';
+export { default as PlasmaURLs } from './PlasmaService/PlasmaURLs.js';
